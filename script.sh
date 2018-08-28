955cdbacda8b5fbf90006d3faa347addf7ca322e
Kevin Rajan <veeman961@gmail.com>
2018-08-27 21:13:08 -0700

    Does anyone read this? I'll be at the coffee shop accross the street. (http://whatthecommit.com)

---
+++ script.sh
@@ -0,0 +1,30 @@
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install hyper
+brew cask install hyper
+
+# install zsh, oh-my-zsh, zsh-autosuggestions
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install zsh-autosuggestions
+
+# install window manager
+brew tap crisidev/homebrew-chunkwm
+brew install chunkwm
+
+# install random packages
+brew install cowsay sl
+
+# clone dotfiles
+git clone https://github.com/lesseradmin/.files ~/.files
+
+# create symlinks
+ln -s ~/.files/.zsh/.zshrc ~/.zshrc
+ln -s ~/.files/.oh-my-zsh ~/.oh-my-zsh
+ln -s ~/.files/.vim/ ~/.vim
+ln -s ~/.files/.vim/.vimrc ~/.vimrc
+ln -s ~/.files/.chunkwm/.chunkwmrc ~/.chunkwmrc
+ln -s ~/.files/.khd/.khdrc-chunkwm ~/.khdrc
+ln -s ~/.files/.gitconfig ~/.gitconfig
+
