2dd8b274024c644fb174b4cac982ce0ecde0f2c4
lesseradmin <veeman961@gmail.com>
2024-12-04 00:30:42 -0600

    Rush B!

---
+++ .i3/README.md
@@ -0,0 +1,13 @@
+# .i3
+My i3 configuration
+
+# Packages to install
+
+1.) dependencies - (libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev)
+
+2.) i3-gaps (compile and install from cloned git repository)
+
+3.) pactl (may already be installed)
+
+4.) 
+
---
+++ .i3/install-script.sh
@@ -0,0 +1,15 @@
+# installs the dependencies for i3-gaps
+sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
+
+# clone the repository
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+../configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
